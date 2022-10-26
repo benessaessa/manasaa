26cc0aa4a7a1a7deb68de9f4a48de40309b2ec54
MOHAMED ESSA <essa@pencil-designs.com>
2022-10-26 11:43:33 +0200

    project manasaa

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import {
+  Container,
+} from "reactstrap"
+
+const Dashboard = props => {
+
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <Container fluid>
+
+          <h3>Welcome to Dashboard</h3>
+
+        </Container>
+      </div>
+    </React.Fragment>
+  )
+}
+export default Dashboard;
---
+++ src/pages/Items/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container } from 'reactstrap';
+
+const Items = props => {
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <Container fluid>
+
+          <h3>Welcome to Items</h3>
+
+        </Container>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Items
