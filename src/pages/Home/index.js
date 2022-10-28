bab7bf0a4092635d66dc4de393548bda798c8060
MOHAMED ESSA <essa@pencil-designs.com>
2022-10-28 17:03:08 +0200

    manassa

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -34,7 +34,7 @@ const Home = () => {
               </Link>
               <Link to="/login">
                 <button className="height56 btnGetStarted px-5 border-0 fw-bold">
-                  Login to website esssa
+                  Login to website
                 </button>
               </Link>
             </div>
